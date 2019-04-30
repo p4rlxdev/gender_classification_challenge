8a043d659ef78ede2f00966e478567e3e7594aeb
Aylex Riom <44292297+p4rlxdev@users.noreply.github.com>
2019-04-29 21:20:43 -0700

    Add files via upload

--- gender_classification.py
+++ gender_classification.py
@@ -3,9 +3,9 @@ from sklearn import tree
 #[height, weight, shoe size]
 X = [[181,80,44], [184,74,42], [190,77,43], [175, 72,41]]
 
+#Genders
 Y = ['male', 'female', 'male','female']
 
-#Genders
 
 
 clf = tree.DecisionTreeClassifier()
